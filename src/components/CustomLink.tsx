4e901b9e502a22443b0c0cf8025487c769f64d67
Gregory Rosenbaum <bitard3d@gmail.com>
2024-03-08 04:03:26 +0200

    some fixes

---
+++ src/components/CustomLink.tsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import {Nav} from "react-bootstrap";
+import {Link, useLocation} from "react-router-dom";
+
+const CustomLink = ({children, to}: { children: PropTypes.ReactNodeLike, to: string }) => {
+
+    const location = useLocation();
+    const match = location.pathname === to;
+
+    return (
+        <Link className={match ? "nav-link active" : "nav-link"} to={to}>{children}</Link>
+    );
+};
+
+CustomLink.propTypes = {
+    children: PropTypes.node.isRequired,
+    to: PropTypes.object.isRequired
+};
+
+export default CustomLink;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,8 @@ import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
 import {useStores} from "../stores/RootStore.ts";
 import Switch from "./Switch.tsx";
 import Sandwich from "./Sandwich.tsx";
+import CustomLink from "./CustomLink.tsx";
+import {Link, Outlet} from "react-router-dom";
 
 const Layout = observer(() => {
 
@@ -13,29 +15,23 @@ const Layout = observer(() => {
             <Navbar expand="sm" className="bg-body-tertiary">
                 <Container>
                     <Navbar.Brand href="#home">
-                        <Switch name={"theme-switch"} handler={uiStore.switchTheme} value={uiStore.theme==="dark"}/>
+                        <Switch name={"theme-switch"} handler={uiStore.switchTheme} value={uiStore.theme === "dark"}/>
                     </Navbar.Brand>
-
-                    <Navbar.Toggle as={"button"} aria-controls={"basic-navbar-nav"} children={<Sandwich />} />
+                    <Navbar.Toggle as={"button"} aria-controls={"basic-navbar-nav"} children={<Sandwich/>}/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider/>
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
+                            <CustomLink to={"/"}>Home</CustomLink>
+                            <CustomLink to={"/search"}>Search</CustomLink>
+                            <CustomLink to={"/private"}>My GitHub</CustomLink>
+                            <CustomLink to={"/settings"}>Settings</CustomLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <Container>
+                <Outlet/>
+            </Container>
+
         </div>
 
         // <div className="row">
